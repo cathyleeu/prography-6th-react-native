8984afe6cb84bbc2775e61ff3491d467a4c66fc0
Cathy Lee <leeyu823@gmail.com>
2020-03-05 20:24:23 +0900

    build : install dev-tools and setup babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@molecules': './src/components/molecules',
+          '@organisms': './src/components/organisms',
+          '@navigations': './src/navigations',
+          '@scenes': './src/scenes',
+          '@helpers': './src/helpers',
+          '@styles': './src/styles',
+          '@utils': './src/utils',
+        },
+      },
+    },
+  },
 };
